76982712ba16222b5bc04e102c3d78e7e545ba9b
John Tao <80338582+johntao@users.noreply.github.com>
2025-02-03 20:49:34 +0800

    refactor: random item selection to use a more descriptive variable name

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const qqq = ['foo', 'bar', 'baz', 'qux', 'quux', 'corge', 'grault', 'garply', 'waldo', 'fred', 'plugh', 'xyzzy', 'thud', 'boom', 'bang'];
+const arr = ['foo', 'bar', 'baz', 'qux', 'quux', 'corge', 'grault', 'garply', 'waldo', 'fred', 'plugh', 'xyzzy', 'thud', 'boom', 'bang'];
 export function foo() {
-  const idx = Math.floor(Math.random() * qqq.length);
-  return qqq[idx];
+  const idx = Math.floor(Math.random() * arr.length);
+  return arr[idx];
 }
