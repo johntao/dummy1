ea1a6b3ca90a28718d7558295bd40ad07b46233c
John Tao <80338582+johntao@users.noreply.github.com>
2025-02-03 18:57:34 +0800

    add random item selection function from predefined array

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello, World!");
+const qqq = ['foo', 'bar', 'baz', 'qux', 'quux', 'corge', 'grault', 'garply', 'waldo', 'fred', 'plugh', 'xyzzy', 'thud', 'boom', 'bang'];
+export function foo() {
+  const idx = Math.floor(Math.random() * qqq.length);
+  return qqq[idx];
+}
